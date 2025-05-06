351c927469f55538d70ca2ee0769e46f95f602a1
Rohan Gautam <rohan1gautam@gmail.com>
2025-05-06 19:48:03 +0800

    very elegant structure

--- src/igp.py
+++ src/igp.py
@@ -5,7 +5,7 @@ import numpyro.distributions as dist
 from .utils import evaluate_kernel, integrate_kernel, invert
 
 
-def eiv_igp(x, x_std, y, y_std, x_star):
+def eiv_igp(x, x_std, y, y_std, x_star, simplify=False):
     alpha = numpyro.sample("beta0", dist.Normal(0, 1000.0))
     p = numpyro.sample("p", dist.Uniform(0.0, 1.0))
     # p = numpyro.deterministic("p", 0.005)
@@ -13,9 +13,6 @@ def eiv_igp(x, x_std, y, y_std, x_star):
     kernel_variance = 1 / kernel_precision
     microscale_std = numpyro.sample("sigma", dist.Uniform(0.01, 1.0))
     params = {"p": p, "kappa": 1.99, "variance": kernel_variance}
-
-    # x_true = numpyro.sample("x_true", dist.Normal(x, x_std).to_event(1))
-    x_true = numpyro.sample("x_true", dist.Normal(0, 1e3**0.5).expand([len(x)]))
 
     C_w = evaluate_kernel(x_star[:, None], x_star[None, :], params)
     C_w += jnp.eye(len(x_star)) * 1e-5  # jitter
@@ -25,15 +22,21 @@ def eiv_igp(x, x_std, y, y_std, x_star):
 
     Cw_inv_w_m = jnp.matmul(invert(C_w), w_m)
 
-    # integral approximation
-    K_hw = integrate_kernel(x_true, x_star, params)
-    h_x = jnp.matmul(K_hw, Cw_inv_w_m)
+    if simplify:
+        # quadrature over fixed x
+        K_hw = integrate_kernel(x, x_star, params)
+    else:
+        # quadrature over estimated x (EIV)
+        # x_true = numpyro.sample("x_true", dist.Normal(x, x_std).to_event(1))
+        x_true = numpyro.sample("x_true", dist.Normal(0, 1e3**0.5).expand([len(x)]))
+        K_hw = integrate_kernel(x_true, x_star, params)
+        numpyro.sample(
+            "x",
+            dist.Normal(loc=x_true, scale=x_std).to_event(1),
+            obs=x,
+        )
 
-    numpyro.sample(
-        "x",
-        dist.Normal(loc=x_true, scale=x_std).to_event(1),
-        obs=x,
-    )
+    h_x = jnp.matmul(K_hw, Cw_inv_w_m)
 
     numpyro.sample(
         "y",
